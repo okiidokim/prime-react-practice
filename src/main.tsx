906c8d2f3d36391a84a735cf96b59a0c5f376e68
김도현 <127471879+okiidokim@users.noreply.github.com>
2026-02-07 00:23:31 +0900

    chore: file 경로 alias 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { PrimeReactProvider } from 'primereact/api';
+import '@/index.css';
 import 'primereact/resources/themes/lara-light-purple/theme.css';
 import 'primereact/resources/primereact.min.css';
-import './index.css';
-import { router } from './router';
+import { router } from '@/router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
