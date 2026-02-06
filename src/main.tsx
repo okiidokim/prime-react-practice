4714c29d377fc21e3695f2c0b42cf66ee41f5721
김도현 <127471879+okiidokim@users.noreply.github.com>
2026-02-06 23:41:24 +0900

    chore: primeReact 스타일 적용되도록 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/lara-light-purple/theme.css';
+import 'primereact/resources/primereact.min.css';
 import './index.css';
 import { router } from './router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <PrimeReactProvider>
+      <RouterProvider router={router} />
+    </PrimeReactProvider>
   </StrictMode>,
 );
